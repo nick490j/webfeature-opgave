9cec3f8f8ff1237523df828e0b261bc36dfa11f7
Thordis Kara Valsdottir <thor9706@stud.kea.dk>
2022-09-13 12:24:39 +0200

    end

--- src/main.js
+++ src/main.js
@@ -7,3 +7,12 @@ animate(".mereanimate",
   { opacity: [ 0.4, 1, 0, 0.8], scale: [0.95, 1, 0.7, 0.9] }, 
   { delay: 5, duration: 4, direction: "alternate", repeat: Infinity }
 )
+
+//Horizontal scroll
+const items = document.querySelectorAll("#horisontalliste li");
+scroll(
+   animate("#horisontalliste", {
+     transform: ["none", `translateX(-${items.length - 1}00vw)`]
+   }),
+   { target: document.querySelector("#horisontalscrollsection") }
+ ); 
